f85a6f537e3d355deaa3f3c146dbf125a9a60d26
whs465 <whs465@gmail.com>
2021-11-02 08:14:32 +0300

    round 3

--- js/plugins.init.js
+++ js/plugins.init.js
@@ -235,7 +235,7 @@ try {
 try {
   if (document.getElementById('days')) {
     // The data/time we want to countdown to
-    var eventCountDown = new Date('November 1, 2021 23:59:00').getTime()
+    var eventCountDown = new Date('November 30, 2021 24:00:00').getTime()
 
     // Run myfunc every second
     var myfunc = setInterval(function () {
