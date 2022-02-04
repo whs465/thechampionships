1012352d52972ff56978269220e2e4b721344d9a
whs465 <whs465@gmail.com>
2022-02-04 12:07:15 +0300

    round 5 janno

--- js/app.js
+++ js/app.js
@@ -27,7 +27,7 @@ function fn() {
     setTimeout(() => {
       document.getElementById('preloader').style.visibility = 'hidden'
       document.getElementById('preloader').style.opacity = '0'
-    }, 1000)
+    }, 1200)
   }
   // Menus
   activateMenu()
