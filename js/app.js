3f6e53396033b75a6f7916e204822918515fb3e2
whs465 <whs465@gmail.com>
2022-02-25 07:57:35 +0300

    round mickey

--- js/app.js
+++ js/app.js
@@ -486,7 +486,7 @@ async function renderScores() {
     }
 
     playerOut =
-      scores.data[i][6] === 'Mohamed'
+      scores.data[i][6] === 'Mickey'
         ? `class="text-black-50  text-decoration-line-through"`
         : ``
 
