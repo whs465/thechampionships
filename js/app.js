d647848e05ae4112266c668606f99da1bafac52c
whs <whs465@users.noreply.github.com>
2022-03-01 21:03:29 +0300

    Update app.js
    Delete Sami update

--- js/app.js
+++ js/app.js
@@ -486,7 +486,7 @@ async function renderScores() {
     }
 
     playerOut =
-      scores.data[i][6] === 'Mickey' ¦¦ scores.data[i][6] === 'Sami M.'
+      scores.data[i][6] === 'Mickey'
         ? `class="text-black-50  text-decoration-line-through"`
         : ``
 
